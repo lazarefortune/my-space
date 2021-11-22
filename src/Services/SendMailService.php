554c24036cdbfe07942e1d45a83b0da3fd6e9a14
Lazare Fortune <30579785+lazarefortune@users.noreply.github.com>
2021-11-22 21:25:33 +0100

    chore : refactoring Modification du SendMailService

--- src/Services/SendMailService.php
+++ src/Services/SendMailService.php
@@ -22,19 +22,22 @@ class SendMailService
         array $context 
     ) : void
     {
-        
-        $email = (new TemplatedEmail())
-                ->subject( $subject )
-                // On attribue les expéditeurs
-                ->from( $from )
-                // On attribue les destinataires
-                ->to( $to )
-                // On crée le texte avec la vue
-                ->htmlTemplate( 'layouts/emails/'. $template .'html.twig' )
-                // On ajoute les valeurs de la vue
-                ->context( $context );
-
-        $this->mailer->send( $email );
+        $from = $from[0];
+        foreach ($to as $value) {
+            
+            $email = (new TemplatedEmail())
+                    ->subject( $subject )
+                    // On attribue les expéditeurs
+                    ->from( $from )
+                    // On attribue les destinataires
+                    ->to( $value )
+                    // On crée le texte avec la vue
+                    ->htmlTemplate( 'layouts/emails/'. $template .'html.twig' )
+                    // On ajoute les valeurs de la vue
+                    ->context( $context );
+    
+            $this->mailer->send( $email );
+        }
     }
 
 }
